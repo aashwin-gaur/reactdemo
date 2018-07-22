d1167d769ccf07b7adb7336a3ca1da3a9b0a295a
Aashwin Gaur <gaur.aashwin95@gmail.com>
2018-07-22 14:46:21 +1000

    initial commit

---
+++ src/template/header/header.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';  
+import './header.css'
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+      }
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
+      render() {
+        return (
+          <div>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">ReactDemo</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/components/">Components</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/">Blog</NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Dropdown
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>
+                      Menu Item 1 
+                      </DropdownItem>
+                      <DropdownItem>
+                      Menu Item 2 
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem>
+                        Reset
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </div>
+        );
+      }
+    }
+export default Header;
